347ace0fbbffa84466f01e4a88d5092c93b27af2
merahulshedge95 <merahulshedge95@gmail.com>
2021-10-12 23:44:07 +0530

    tox and package build changes

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup,find_packages
+
+setup(
+    name= "src",
+    version= "0.0.1",
+    description="First package on wineQ check",
+    author="rahulshedge555",
+    package=find_packages(),
+    licencse="MIT"
+)
