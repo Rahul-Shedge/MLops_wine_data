22a00160c3d1c8b27a41b51bd66af3dda895bfea
merahulshedge95 <merahulshedge95@gmail.com>
2021-10-19 13:43:04 +0530

    github workflow added

--- test/test_config.py
+++ test/test_config.py
@@ -6,7 +6,7 @@ class NotInRange(Exception):
         super().__init__(self.message)
 
 def test_generic():
-    a = 5
+    a = 10
     with pytest.raises(NotInRange):
         if a is not range(10,20):
             raise NotInRange
